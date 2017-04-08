8fc61c4248a1f65d3978ed952bd472974f881495
alanyeh20001 <alanyeh20001@hotmail.com>
2017-04-09 02:22:19 +0800

    delete unused HomeContainer

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -3,26 +3,16 @@ import React from 'react';
 class Home extends React.Component {
 	static get propTypes() {
 		return {
-			users: React.PropTypes.array,
 		};
 	}
 	render() {
-		const { users } = this.props;
 		const style = require('./Home.scss');
 		// In this place, funyee represents the file path.
-		const funyee = require('./funyee.png');
+		//
+		// const funyee = require('./funyee.png');
 		return (
 			<div>
-				{
-					users.map( (item, index) => {
-						return (
-							<div key={index} className={style.box}>
-								<img className={style.image} src={funyee} />
-								<span className={style.name}>{item.name}</span>
-							</div>
-						);
-					})
-				}
+			    Home
 			</div>
 		);
 	}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import HomeContainer from '../containers/HomeContainer/HomeContainer.js';
+import Home from '../components/pages/Home/Home.js';
 import MapDemo from '../components/pages/MapDemo/MapDemo.js';
 import Layout from '../components/layouts/Layout/Layout.js';
 
 export default (
 	<Route path="/" component={Layout}>
-		<IndexRoute component={HomeContainer} />
+		<IndexRoute component={Home} />
 		<Route path='map' component={MapDemo} />
 	</Route>
 );
