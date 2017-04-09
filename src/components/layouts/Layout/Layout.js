615d4b87b04d065c56c05b2a7788b2c984d367dc
alanyeh20001 <alanyeh20001@hotmail.com>
2017-04-09 23:08:45 +0800

    Complete sidebar w/o graph

--- src/components/layouts/Layout/Layout.js
+++ src/components/layouts/Layout/Layout.js
@@ -8,7 +8,7 @@ class Layout extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			menuSwitch: false
+			menuSwitch: true
 		};
 	}
 	_onClick() {
@@ -20,7 +20,9 @@ class Layout extends React.Component {
 		const style = require('./Layout.scss');
 		const menuIcon = require('./hamburger.png');
 		const logo = require('./logo.png');
-		const menu = require('./menu.png');
+
+		const menuClass = this.state.menuSwitch === true ? style.menu : style.menu + " " + style.menuHidden
+		console.log(menuClass)
 		return (
 			<div>
 				<nav className={style.navbar}>
@@ -29,8 +31,28 @@ class Layout extends React.Component {
 						<a href="/" className={style.logoIcon}><img src={logo}/></a>
 					</div>
 				</nav>
-				<div className={this.state.menuSwitch ? style.menu : style.menuHidden}>
-					<img src={menu}/>
+				<div className={menuClass}>
+					<div className={style.description}>
+						<ul>
+							<li>about</li>
+							<li>contact</li>
+						</ul>
+					</div>
+					<hr/>
+					<div className={style.text}>filter</div>
+					<div className={style.form}>
+						<select>
+							<option>台北市</option>
+							<option>台中市</option>
+							<option>台南市</option>
+							<option>高雄市</option>
+						</select>
+						<select>
+							<option>中山區</option>
+							<option>大安區</option>
+							<option>信義區</option>
+						</select>
+					</div>
 				</div>
 				<div className={style.content}>
 					{this.props.children}
