0b6e24aa5a2704133497fe11ddbe5db070a59357
alanyeh20001 <alanyeh20001@hotmail.com>
2017-04-09 13:50:16 +0800

    Complete index

--- src/components/layouts/Layout/Layout.js
+++ src/components/layouts/Layout/Layout.js
@@ -5,13 +5,33 @@ class Layout extends React.Component {
 		return {
 		};
 	}
+	constructor(props) {
+		super(props);
+		this.state = {
+			menuSwitch: false
+		};
+	}
+	_onClick() {
+		this.setState({
+			menuSwitch: !this.state.menuSwitch
+		});
+	}
 	render() {
 		const style = require('./Layout.scss');
+		const menuIcon = require('./hamburger.png');
 		return (
 			<div>
 				<nav className={style.navbar}>
-					<a href="https://www.google.com">HouseHack</a>
+					<div>
+						<a href="/"></a>
+						<a href="#" className={style.menuIcon} onClick={this._onClick.bind(this)}><img src={menuIcon}/></a>
+					</div>
 				</nav>
+				<div className={this.state.menuSwitch ? style.menu : style.menuHidden}>
+					<ul>
+						<li><a href="#about">About</a></li>
+					</ul>
+				</div>
 				<div className={style.content}>
 					{this.props.children}
 				</div>
